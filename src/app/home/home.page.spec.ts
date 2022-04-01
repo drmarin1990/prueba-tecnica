112f41cc3ddf1159340263744b20dea85fe4b93c
diego <dr.marin1990@gmail.com>
2022-04-01 17:35:37 +0200

    feat: desarrollo completado prueba tecnica

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
@@ -6,19 +7,96 @@ import { HomePage } from './home.page';
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
-
+  
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), ReactiveFormsModule, FormsModule]
     }).compileComponents();
+  }));
 
+  beforeEach(()=>{
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('call doLogin', ()=>{
+    beforeEach(()=>{
+      spyOn(console, 'log');
+    });
+
+    describe('with valid credentials', ()=>{
+      beforeEach(()=>{
+        component.loginForm.controls['email'].setValue('test@gmail.com');
+        component.loginForm.controls['password'].setValue('12345');
+        component.doLogin();
+      });
+
+      it('should log ok', ()=>{
+        expect(console.log).toHaveBeenCalledWith('OK');
+      });
+      it('should be valid email', ()=>{
+        expect(component.loginForm.controls['email'].valid).toBeTruthy();
+      });
+      it('should be valid password', ()=>{
+        expect(component.loginForm.controls['password'].valid).toBeTruthy();
+      });
+      it('should set loginForm.valid to true', ()=>{
+        expect(component.loginForm.valid).toBeTruthy();
+      });
+    
+    });
+    
+    describe('with invalid credentials', ()=>{
+      beforeEach(()=>{
+      });
+      describe('no valid email', ()=>{
+        beforeEach(()=>{
+          component.loginForm.controls['email'].setValue('test');
+          component.loginForm.controls['password'].setValue('12345');
+          component.doLogin();
+        });
+
+        it('should log error', ()=>{
+          expect(console.log).toHaveBeenCalledWith('Por favor rellena los campos correctamente');
+        });
+        it('should be invalid email', ()=>{
+          expect(component.loginForm.controls['email'].valid).toBeFalsy();
+        });
+        it('should be valid password', ()=>{
+          expect(component.loginForm.controls['password'].valid).toBeTruthy();
+        });
+        it('should set loginForm.valid to false', ()=>{
+          expect(component.loginForm.valid).toBeFalsy();
+        });
+      });
+      describe('no valid password', ()=>{
+        beforeEach(()=>{
+          component.loginForm.controls['email'].setValue('test@gmail.com');
+          component.loginForm.controls['password'].setValue('1234');
+          component.doLogin();
+        });
+
+        it('should log error', ()=>{
+          expect(console.log).toHaveBeenCalledWith('Por favor rellena los campos correctamente');
+        });
+        it('should be valid email', ()=>{
+          expect(component.loginForm.controls['email'].valid).toBeTruthy();
+        });
+        it('should be invalid password', ()=>{
+          expect(component.loginForm.controls['password'].valid).toBeFalsy();
+        });
+        it('should set loginForm.valid to false', ()=>{
+          expect(component.loginForm.valid).toBeFalsy();
+        });
+    
+      });
+    }); 
+
   });
 });
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,28 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  loginForm: FormGroup;
+  isSubmitted = false;
 
+  constructor(
+    public formBuilder: FormBuilder
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+    });
+  }
+
+  doLogin() {
+    this.isSubmitted = true;
+    if (this.loginForm.valid) {
+      console.log('OK');
+    } else {
+      console.log('Por favor rellena los campos correctamente');
+    }
+  }
+
+  get errorControl() {
+    return this.loginForm.controls;
+  }
 }
